719e4cfadfbb6a454b8c2cf71820dcf66597271a
Leonel Puertas <leonel.puertas@incluit.com>
2020-10-17 18:09:33 -0300

    Agregar archivos de proyecto.

---
+++ RedHopfield/Patron.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RedHopfield
+{
+    public class Patron
+    {
+        private List<int> patron { get; set; }
+        public Patron(int a, int b, int c, int d)
+        {
+            patron = new List<int>() { a, b, c, d };
+        }
+        public List<List<int>> CalcularMatriz()
+        {
+            List<List<int>> matriz = new List<List<int>>();
+            List<int> vector = new List<int>();
+            foreach (var componente in patron)
+            {
+                foreach (var componente2 in patron)
+                {
+                    vector.Add(componente * componente2);
+                }
+                matriz.Add(vector);
+                vector = new List<int>();
+            }
+            return matriz;
+        }
+        public void Imprimir()
+        {
+            string linea;
+            foreach (var itemI in CalcularMatriz())
+            {
+                linea = "[";
+                foreach (var itemJ in itemI)
+                {
+                    linea += itemJ + ", ";
+                }
+                linea = linea.Remove(linea.Length - 1) + "]";
+
+                Console.WriteLine(linea);
+            }
+        }
+    }
+}
---
+++ RedHopfield/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RedHopfield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //entrenamiento
+            List<Patron> patrones = new List<Patron>();
+            patrones.Add(new Patron(1, 1, 1, -1));
+            patrones.Add(new Patron(-1, -1, -1, 1));
+
+            //Aprendizaje
+            Matriz redHopfield = new Matriz(patrones);
+            redHopfield.CalcularMatrizPesos();
+            redHopfield.Imprimir();
+        }
+    }
+}
